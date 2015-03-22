5d4244f0786a58de0ddf52156f94469134b052d0
Komandos28 <marcin_1992@onet.eu>
2015-03-22 12:12:40 +0100

    poprawki sklepu i wyglad caly

--- src/AppBundle/Controller/ProductsController.php
+++ src/AppBundle/Controller/ProductsController.php
@@ -4,6 +4,8 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+
 use AppBundle\Entity\Category;
 
 class ProductsController extends Controller
@@ -11,25 +13,22 @@ class ProductsController extends Controller
     /**
      * @Route("/produkty/{id}", name="products_list", defaults={"id" = false})
      */
-    public function indexAction(Category $category = null)
+    public function indexAction(Request $request, Category $category = null)
     {
-        if ($category) {
-            $products = $this->getDoctrine()
-                    ->getRepository('AppBundle:Product')
-                    ->findBy([
-                        'category' => $category,
-                        ]);
-                    
-        } else {
-            $products = $this->getDoctrine()
-                    ->getRepository('AppBundle:Product')
-                    ->findAll();
-        }
+        $getProductsQuery = $this->getDoctrine()
+            ->getRepository('AppBundle:Product')
+            ->getProductsQuery($category);
         
+        $paginator = $this->get('knp_paginator');
+        $products = $paginator->paginate(
+            $getProductsQuery,
+            $request->query->get('page', 1),
+            8
+        );
+
         return $this->render('products/index.html.twig', [
             'products' => $products,
         ]);
     }
-    
-    
+
 }
